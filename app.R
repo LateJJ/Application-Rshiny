7aafa78cc686e2c502025b2ea5e2d7316421e08d
Laté LAWSON <115092705+LateJJ@users.noreply.github.com>
2022-12-13 02:25:03 +0100

    Update app.R

--- app.R
+++ app.R
@@ -4,91 +4,118 @@ library(shinydashboard)
 library(shinyWidgets)
 library(ggplot2)
 library(forecast)
-library(cowplot)
 library(lubridate)
 library(dplyr)
-library(fpp2)
+#library(fpp2)
 
-conso =  read.csv("C:/Users/sguest/Desktop/R av/bilan-electrique-transpose.csv",header = TRUE,sep = ";",encoding = "UTF-8")
+# Importation de la table
 
-conso_trier = conso %>% arrange(Jour)
+conso =  read.csv("bilan-electrique-transpose.csv", header = TRUE,sep = ";")
+conso_trier = conso %>% arrange(Jour) %>% 
+  rename(Puissance_moy = Puissance.moyenne.journalière..W. )
+
+# Conso avant et après covid
 
 conso_train = conso_trier %>% filter(Jour <= "2020-03-17")
-
 vrai_conso = conso_trier %>% filter(Jour >= "2020-03-18" & Jour <= "2021-12-30" )
 
-vect_categorie = unique(conso$Catégorie.client)
 
-################################################################################
-#  Fonction pour la prédiction
+# Tableau des consommations avant la covid par Catégories clients
 
-Estimation = function(table,categorie_client){
-  ts = ts( table %>% select(Puissance.moyenne.journalière..W.),
-           start= c(table$Jour[1] %>% year(),1),frequency = 7 )
-  fit.arima = auto.arima(ts,d=0,D=1,stepwise = FALSE,
-                         approximation = FALSE,trace = TRUE)
+conso_entrep =  conso_train %>% filter(Catégorie.client == "Entreprises" )
+conso_resid =  conso_train %>% filter(Catégorie.client == "Résidentiels" )
+conso_profe =  conso_train %>% filter(Catégorie.client == "Professionnels" )
+conso_pme_pmi =  conso_train %>% filter(Catégorie.client == "PME / PMI" )
+conso_pmjct =  conso_train %>%
+  filter(Catégorie.client == "Puissance moyenne journalière des consommations télérelevées à courbe de charge (W)" )
+conso_pmjch =  conso_train %>%
+  filter(Catégorie.client == "Puissance moyenne journalière des consommations HTA télérelevées (W)" )
+
+
+# Tableau des consommations après la covid par Catégories clients
+  
+vrai_conso_entrep = vrai_conso %>% filter(Catégorie.client =="Entreprises" )
+vrai_conso_resid =  vrai_conso %>% filter(Catégorie.client == "Résidentiels" )
+vrai_conso_profe =  vrai_conso %>% filter(Catégorie.client == "Professionnels" )
+vrai_conso_pme_pmi =  vrai_conso %>% filter(Catégorie.client == "PME / PMI" )
+vrai_conso_pmjct =  vrai_conso %>%
+  filter(Catégorie.client == "Puissance moyenne journalière des consommations télérelevées à courbe de charge (W)" )
+vrai_conso_pmjch =  vrai_conso %>%
+  filter(Catégorie.client == "Puissance moyenne journalière des consommations HTA télérelevées (W)" )
+
+
+# Fonction pour la prédiction avec arima
+# Les ordres de arima trouvé à partir de la fonction auto.arima 
+
+Estim = function(table1,table2){
+  fit.arima = arima(table1 %>% select(Puissance_moy), order = c(1,0,2),
+                    list(order = c(2,1,0), period=7), include.mean = FALSE,
+                    method= "CSS-ML")
   fcst = forecast(fit.arima,h=653)
-  tab_predit = fcst$mean
-  return(tab_predit)
+  tab_predit = as.numeric(fcst$mean)
+  ICupper <- as.numeric(fcst$upper[,2]) # à 95%
+  IClower <- as.numeric(fcst$lower[,2]) # à 95%
+  tab = cbind(table2,tab_predit,ICupper,IClower)
+  tab_f = tab %>%
+    mutate(difference = (Puissance_moy - tab_predit))
+  return(tab_f)
 }
 
-################################################################################
+# Application de la fonction Estim
+entreprise = Estim(conso_entrep,vrai_conso_entrep)
+professionnel = Estim(conso_profe,vrai_conso_profe)
+residentiel = Estim(conso_resid,vrai_conso_resid)
+pme_pmi = Estim(conso_pme_pmi,vrai_conso_pme_pmi)
+pmjch = Estim(conso_pmjch,vrai_conso_pmjch)
+pmjct = Estim(conso_pmjct,vrai_conso_pmjct)
 
-# Fonction get_table
+#
+complete = rbind(entreprise,professionnel,residentiel,
+                 pme_pmi,pmjch,pmjct)
 
-get_table = function(data1, data2){
-  data3 <- cbind(data1,data2)
-  data <- cbind(data3,data3[2]-data[3])
-  return(data)
-}
-################################################################################
-
+vect_categorie = unique(conso$Catégorie.client)
 
 
 # Define UI for application 
 ui <- fluidPage(
-    useShinydashboard(),
-
-    # Application title
-    titlePanel("Impact de la crise sanitaire sur les consommations d’électricité"),
-    
-    # Sidebar with a slider input for categories
-    sidebarLayout(
-      sidebarPanel(
-        selectInput(inputId = "categorie",
-                    label = "Choisissez la catégorie",
-                    choices = vect_categorie,
-                    selected = "Entreprises"),
-        
-        
-        # Select date range to be plotted
-        dateRangeInput("date", strong("Choissisez une période"),
-                       start = "2020-03-17", end = "2021-12-30",
-                       min = "2020-03-17", max = "2021-12-30"),
-        
-        downloadButton("downloadData", "Download")
-        
+  useShinydashboard(),
+  
+  # Application title
+  titlePanel("Impact de la crise sanitaire sur les consommations d’électricité"),
+  
+  # Sidebar with a slider input for categories
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "categorie",
+                  label = "Choisissez la catégorie",
+                  choices = vect_categorie,
+                  selected = "Entreprises"),
+      
+      # Select date range to be plotted
+      dateRangeInput("date", strong("Choissisez une période"),
+                     start = "2020-03-17", end = "2020-07-30",
+                     min = "2020-03-17", max = "2021-12-30"
       ),
       
-     
-      mainPanel(
-        textOutput({"categorie_aff"}),
-    
-        dataTableOutput({"mon_df"}),
-        
-        plotOutput(outputId = "lineplot", height = "300px"),
-        textOutput(outputId = "desc"),
-        
-        #dataTableOutput({"mon_df"}),
-        #plotOutput({"repartition"})
-        valueBoxOutput("vbox1", width = 2),
-        valueBoxOutput("vbox2", width = 2),
-        valueBoxOutput("vbox3", width = 2)
-        
-        
-      ),  
+      # Bouton download
+      downloadButton("downloadData", "Download")
       
-    )
+    ),
+    
+    mainPanel(
+      textOutput({"categorie_aff"}),
+      
+      dataTableOutput({"mon_df"}),
+      
+      plotOutput(outputId = "lineplot", height = "300px"),
+      textOutput(outputId = "desc"),
+      
+      valueBoxOutput("vbox1", width = 8),
+      valueBoxOutput("vbox2", width = 8),
+      valueBoxOutput("vbox3", width = 8)
+      
+    ),  
+  )
 )
 
 # Define server logic required to draw a histogram
@@ -96,61 +123,53 @@ server <- function(input, output) {
   
   output$categorie_aff <- renderText({input$categorie})
   
-  ####
+  ## Datatable
   get_vrai_table <- reactive({
-    
-     vrai_conso %>%
-      filter(vrai_conso$Catégorie.client == input$categorie, 
+    complete %>%
+      filter(complete$Catégorie.client == input$categorie,
              Jour >= as.POSIXct(input$date[1]) & Jour <= as.POSIXct(input$date[2]
-             )) %>% 
-      select(-Catégorie.client)
-
+             )) %>% select(-Catégorie.client)
   })
+  output$mon_df <- renderDataTable(({get_vrai_table()[,-c(4,5)]}))
   
-
-  
-  output$mon_df <- renderDataTable(({get_vrai_table()}))
-
- 
+  ## Graphique
   output$lineplot <- renderPlot({
-    #plot(x = get_vrai_table()$Jour %>% yday(), y = get_vrai_table()$Puissance.moyenne.journalière..W.,
-     #    xlab = "Date", ylab = "Trend index")
-    ggplot(data = get_vrai_table()) +
-        aes(y = Puissance.moyenne.journalière..W. ,  x= Jour%>% yday()) +
-        geom_line() + xlab("Jour") + ylab("Consommation")
+    ggplot(data = get_vrai_table(), aes(x=Jour %>% yday())) +
+      geom_line(aes(y = Puissance_moy), color = "black") +
+      geom_line(aes(y = tab_predit), color = "red") +
+      geom_line(aes(y = ICupper), color="blue", linetype="dashed") +
+      geom_line(aes(y = IClower), color="blue", linetype="dashed")
     
   })
   
- 
-  output$vbox1 <- shinydashboard::renderValueBox({ 
-    sp <- 42 # Valeur à modifier par la suite
-    shinydashboard::valueBox( sp, "Somme prédite")
+  ## Valuebox
+  output$vbox1 <- shinydashboard::renderValueBox({
+    sp <- sum(get_vrai_table()[3])
+    shinydashboard::valueBox(sp, "Somme prédite en GWh")
   })
-  
-  output$vbox2 <- shinydashboard::renderValueBox({ 
-    sr <- 40 # Valeur à modifier par la suite
-    shinydashboard::valueBox( sr, "Somme réalisée")
-  })
-  
-  output$vbox3 <- shinydashboard::renderValueBox({ 
-    Impact <- 2 # Valeur à modifier par la suite
-    P_Im <- 2/100
 
-    shinydashboard::valueBox( Impact, "Impact")
+  output$vbox2 <- shinydashboard::renderValueBox({
+    sr <- sum(get_vrai_table()[2])
+  shinydashboard::valueBox(sr, "Somme réalisée en GWh")
   })
-  
-  
+
+  output$vbox3 <- shinydashboard::renderValueBox({
+    Impact <- sum(get_vrai_table()[2]) - sum(get_vrai_table()[3])
+  shinydashboard::valueBox(Impact, "Impact en GWh")
+  })
+
+  ## Bouton dowload
   output$downloadData <- downloadHandler(
     filename = function() {
       paste('data-', Sys.Date(), '.csv', sep='')
     },
     content = function(con) {
-      write.csv(data, con)
+      write.csv(get_vrai_table(), con)
     }
   )
-  
+
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
 
