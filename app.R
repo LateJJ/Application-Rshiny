3baf2e4d4fc414480e8fdb9a4deed0ab2ef26b36
Laté LAWSON <115092705+LateJJ@users.noreply.github.com>
2022-12-16 19:45:14 +0100

    Update app.R
    Remplacement de certaines lignes de code par des fonctions.
    Modification du graphique en graphique interactif.

--- app.R
+++ app.R
@@ -1,5 +1,5 @@
 library(shiny)
-library(tidyverse)
+#library(tidyverse)
 library(shinydashboard)
 library(shinyWidgets)
 library(ggplot2)
@@ -7,42 +7,30 @@ library(forecast)
 library(lubridate)
 library(dplyr)
 #library(fpp2)
+library(plotly)
 
 # Importation de la table
 
-conso =  read.csv("bilan-electrique-transpose.csv", header = TRUE,sep = ";")
+conso =  read.csv("bilan-electrique-transpose.csv",
+                  header = TRUE,sep = ";",encoding = "UTF-8")
 conso_trier = conso %>% arrange(Jour) %>% 
   rename(Puissance_moy = Puissance.moyenne.journalière..W. )
 
 # Conso avant et après covid
-
 conso_train = conso_trier %>% filter(Jour <= "2020-03-17")
 vrai_conso = conso_trier %>% filter(Jour >= "2020-03-18" & Jour <= "2021-12-30" )
 
 
-# Tableau des consommations avant la covid par Catégories clients
+# Fonction retournant le tableau des consommations avant la covid selon "Catégories clients"
+conso_avant = function(categorie){
+  conso_train %>% filter(Catégorie.client == categorie)
+}
 
-conso_entrep =  conso_train %>% filter(Catégorie.client == "Entreprises" )
-conso_resid =  conso_train %>% filter(Catégorie.client == "Résidentiels" )
-conso_profe =  conso_train %>% filter(Catégorie.client == "Professionnels" )
-conso_pme_pmi =  conso_train %>% filter(Catégorie.client == "PME / PMI" )
-conso_pmjct =  conso_train %>%
-  filter(Catégorie.client == "Puissance moyenne journalière des consommations télérelevées à courbe de charge (W)" )
-conso_pmjch =  conso_train %>%
-  filter(Catégorie.client == "Puissance moyenne journalière des consommations HTA télérelevées (W)" )
+#  Fonction retournant le tableau des consommations après la covid selon "Catégories clients"
 
-
-# Tableau des consommations après la covid par Catégories clients
-  
-vrai_conso_entrep = vrai_conso %>% filter(Catégorie.client =="Entreprises" )
-vrai_conso_resid =  vrai_conso %>% filter(Catégorie.client == "Résidentiels" )
-vrai_conso_profe =  vrai_conso %>% filter(Catégorie.client == "Professionnels" )
-vrai_conso_pme_pmi =  vrai_conso %>% filter(Catégorie.client == "PME / PMI" )
-vrai_conso_pmjct =  vrai_conso %>%
-  filter(Catégorie.client == "Puissance moyenne journalière des consommations télérelevées à courbe de charge (W)" )
-vrai_conso_pmjch =  vrai_conso %>%
-  filter(Catégorie.client == "Puissance moyenne journalière des consommations HTA télérelevées (W)" )
-
+vrai_conso_covid = function(categorie){
+  vrai_conso %>% filter(Catégorie.client == categorie)
+}
 
 # Fonction pour la prédiction avec arima
 # Les ordres de arima trouvé à partir de la fonction auto.arima 
@@ -53,27 +41,22 @@ Estim = function(table1,table2){
                     method= "CSS-ML")
   fcst = forecast(fit.arima,h=653)
   tab_predit = as.numeric(fcst$mean)
-  ICupper <- as.numeric(fcst$upper[,2]) # à 95%
-  IClower <- as.numeric(fcst$lower[,2]) # à 95%
+  ICupper <- as.numeric(fcst$upper[,2]) # IC à 95%
+  IClower <- as.numeric(fcst$lower[,2]) # IC à 95%
   tab = cbind(table2,tab_predit,ICupper,IClower)
   tab_f = tab %>%
     mutate(difference = (Puissance_moy - tab_predit))
   return(tab_f)
 }
 
-# Application de la fonction Estim
-entreprise = Estim(conso_entrep,vrai_conso_entrep)
-professionnel = Estim(conso_profe,vrai_conso_profe)
-residentiel = Estim(conso_resid,vrai_conso_resid)
-pme_pmi = Estim(conso_pme_pmi,vrai_conso_pme_pmi)
-pmjch = Estim(conso_pmjch,vrai_conso_pmjch)
-pmjct = Estim(conso_pmjct,vrai_conso_pmjct)
-
-#
-complete = rbind(entreprise,professionnel,residentiel,
-                 pme_pmi,pmjch,pmjct)
-
 vect_categorie = unique(conso$Catégorie.client)
+
+complete = c()
+for (i in vect_categorie){
+  estim = Estim(conso_avant(i),vrai_conso_covid(i))
+  complete = rbind(complete,estim)
+}
+
 
 
 # Define UI for application 
@@ -105,14 +88,19 @@ ui <- fluidPage(
     mainPanel(
       textOutput({"categorie_aff"}),
       
+      #plotlyOutput(outputId = "lineplot"),
+      
       dataTableOutput({"mon_df"}),
       
-      plotOutput(outputId = "lineplot", height = "300px"),
-      textOutput(outputId = "desc"),
+      #plotOutput(outputId = "lineplot", height = "300px"),
+      #textOutput(outputId = "desc"),
+      plotlyOutput(outputId = "lineplot", height = "300px"),
       
       valueBoxOutput("vbox1", width = 8),
       valueBoxOutput("vbox2", width = 8),
-      valueBoxOutput("vbox3", width = 8)
+      valueBoxOutput("vbox3", width = 8),
+      valueBoxOutput("vbox4", width = 8)
+      
       
     ),  
   )
@@ -133,38 +121,49 @@ server <- function(input, output) {
   output$mon_df <- renderDataTable(({get_vrai_table()[,-c(4,5)]}))
   
   ## Graphique
-  output$lineplot <- renderPlot({
+  output$lineplot <- renderPlotly({
+    ggplotly(
     ggplot(data = get_vrai_table(), aes(x=Jour %>% yday())) +
       geom_line(aes(y = Puissance_moy), color = "black") +
-      geom_line(aes(y = tab_predit), color = "red") +
+      geom_line(aes(y = tab_predit), color = "orange") +
       geom_line(aes(y = ICupper), color="blue", linetype="dashed") +
-      geom_line(aes(y = IClower), color="blue", linetype="dashed")
+      geom_line(aes(y = IClower), color="blue", linetype="dashed") +
+      xlab("Jour") +
+      ggtitle("Graphique valeurs réalisées & valeurs prédites"), 
+      
+    ) 
     
   })
   
   ## Valuebox
   output$vbox1 <- shinydashboard::renderValueBox({
     sp <- sum(get_vrai_table()[3])
-    shinydashboard::valueBox(sp, "Somme prédite en GWh")
+    shinydashboard::valueBox(sp, "Somme prédite en GWh", color = "orange")
   })
 
   output$vbox2 <- shinydashboard::renderValueBox({
     sr <- sum(get_vrai_table()[2])
-  shinydashboard::valueBox(sr, "Somme réalisée en GWh")
+  shinydashboard::valueBox(sr, "Somme réalisée en GWh",color = "black" )
   })
 
   output$vbox3 <- shinydashboard::renderValueBox({
     Impact <- sum(get_vrai_table()[2]) - sum(get_vrai_table()[3])
-  shinydashboard::valueBox(Impact, "Impact en GWh")
+  shinydashboard::valueBox(Impact, "Impact en GWh", col = "red")
   })
 
+  output$vbox4 <- shinydashboard::renderValueBox({
+    Impact <- sum(get_vrai_table()[2])/sum(get_vrai_table()[3])
+    Impact <- scales::percent(Impact)
+    shinydashboard::valueBox(Impact, "Impact en pourcentage")
+  })
+  
   ## Bouton dowload
   output$downloadData <- downloadHandler(
     filename = function() {
       paste('data-', Sys.Date(), '.csv', sep='')
     },
     content = function(con) {
-      write.csv(get_vrai_table(), con)
+      write.csv(get_vrai_table()[,-c(4,5)], con)
     }
   )
 
@@ -172,4 +171,3 @@ server <- function(input, output) {
 
 # Run the application
 shinyApp(ui = ui, server = server)
-
