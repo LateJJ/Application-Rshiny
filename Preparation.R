baec1935bc1af7419870d2ec16ba8062b72b9188
Laté LAWSON <115092705+LateJJ@users.noreply.github.com>
2023-01-28 00:54:49 +0100

    Update Preparation.R

--- Preparation.R
+++ Preparation.R
@@ -19,3 +19,54 @@ bilan <-  read.csv("bilan-electrique-jour.csv",
 # Modification des noms de variables
 bilan <- bilan %>% rename(Temperature = TempÃ.rature.moyenne.journaliÃ.re.rÃ.alisÃ.e.lissÃ.e..Â.C., 
                           Jour = ï..Jour)
+
+
+# Temperation
+Tempe <- bilan %>% arrange(Jour) %>% select(Jour,Temperature) %>% filter(Jour <= "2021-12-31")
+
+
+# Consommation avant et pendant covid
+
+avant <- conso_trier %>% filter(Jour <= "2020-03-17"  & Jour>="2018-01-14")
+pendant <- conso_trier %>% filter(Jour >= "2020-03-18" & Jour <= "2021-12-30" )
+
+# Temperation avant et pendant covid
+Temp_av_covid <- Tempe %>% filter(Jour <= "2020-03-17") %>% select(-Jour)
+Temp_covid <- Tempe %>% filter(Jour >= "2020-03-18" & Jour <= "2021-12-30") %>% select(-Jour)
+
+
+# Tableau des consommations avant la covid par Catégories clients
+conso_avant <- function(categorie){
+  conso <- avant %>% filter(Categorie.client == categorie)
+  return(cbind(conso,Temp_av_covid))
+}
+
+
+# Tableau des consommations pendant la covid par Catégories clients
+
+vrai_conso_covid <- function(categorie){
+  conso <- pendant %>% filter(Categorie.client == categorie)
+  return(cbind(conso,Temp_covid))
+}
+
+# Modèle de régression linéaire
+Estim <- function(table1,table2){
+  res = lm(Puissance.moy~Temperature, data=table1)
+  prediction <- predict(res,newdata=Temp_covid,
+                        interval = "confidence")
+  prediction=as.data.frame(prediction) # prediction + IC 
+  tab = cbind(table2,prediction)
+  tab_f = tab %>%
+    mutate(difference = (Puissance.moy - tab$fit))
+  return(tab_f)
+}
+
+vect_categorie <- unique(avant$Categorie.client)
+
+complete <- c()
+for (categorie in vect_categorie){
+  estim = Estim(conso_avant(categorie),vrai_conso_covid(categorie))
+  complete = rbind(complete,estim)
+}
+
+
